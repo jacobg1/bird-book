69fe09406b5fbb47a427d10952c981c12e4db605
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-04 17:48:24 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
 
+    resources :places do
+      resources :posts
+    end
 
-  resources :places do
     resources :posts do
       resources :guesses
     end
-  end
+
+
+  # resources :places do
+  #   resources :posts do
+  #     resources :guesses
+  #   end
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
