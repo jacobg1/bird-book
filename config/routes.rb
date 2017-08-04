c5c2ab328f2a7231b2d0981d555206bf6f8ce688
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-04 15:51:49 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+
+  resources :places do 
+    resources :posts do
+      resources :guesses
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
