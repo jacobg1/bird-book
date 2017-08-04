f87f58c51c5562101facc0eb83b6ccbe1bb83c59
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-04 15:26:20 -0400

    MORE SUGAR WATER(more seed data)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Place.destroy_all
+Post.destroy_all
+Guess.destroy_all
 
 my_place = Place.create(country: 'USA', state: 'VA', city: 'Springfield')
 
 first_post = my_place.posts.create(title: 'CRAZY tree', photo_url: 'http://i.imgur.com/IMGCj.jpg')
 
 first_guess = first_post.guesses.create(guess_text: 'its a palm tree')
+second_guess = first_post.guesses.create(guess_text: 'nah its a dogwood dude')
