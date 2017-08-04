b811aed27468da8c6f460b40ff4eee35dd47dd8b
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-04 14:43:36 -0400

    add working seed data!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+my_place = Place.create(country: 'USA', state: 'VA', city: 'Springfield')
+
+first_post = my_place.posts.create(title: 'CRAZY tree', photo_url: 'http://i.imgur.com/IMGCj.jpg')
+
+first_guess = first_post.guesses.create(guess_text: 'its a palm tree')
