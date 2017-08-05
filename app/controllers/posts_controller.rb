4f8556fa0d8633d36b7731c204adf862e46c596b
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-05 12:03:50 -0400

    add new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,25 @@ class PostsController < ApplicationController
     @place = Place.find(params[:place_id])
     @posts = @place.posts.all
   end
+
+  def new
+    @place = Place.find(params[:place_id])
+    @post = @place.posts.new
+  end
+
+  def create
+    @place = Place.find(params[:place_id])
+    @post = @place.posts.create(post_params)
+    redirect_to place_posts_path(@place)
+  end
+
   def show
     @place = Place.find(params[:place_id])
     @post = @place.posts.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :photo_url)
   end
 end
