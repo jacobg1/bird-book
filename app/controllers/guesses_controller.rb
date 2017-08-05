8c2a2679592962fc56ddfcb93d887a5ee645a8cc
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-05 13:57:32 -0400

    add new guess

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -3,4 +3,21 @@ class GuessesController < ApplicationController
     @post = Post.find(params[:post_id])
     @guesses = @post.guesses.all
   end
+
+  def new
+    @post = Post.find(params[:post_id])
+    @guess = @post.guesses.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @place = @post.place
+    @guess = @post.guesses.create(guess_params)
+    redirect_to place_post_path(@place, @post)
+  end
+
+  private
+  def guess_params
+    params.require(:guess).permit(:guess_text)
+  end
 end
