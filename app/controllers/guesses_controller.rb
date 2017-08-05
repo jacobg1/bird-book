829413dc3cbc4b6f39859888ae957d11d2684ea9
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-05 14:20:13 -0400

    finish CRUD for guesses

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -16,6 +16,27 @@ class GuessesController < ApplicationController
     redirect_to place_post_path(@place, @post)
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @guess = Guess.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @place = @post.place
+    @guess = Guess.find(params[:id])
+    @guess.update(guess_params)
+    redirect_to place_post_path(@place, @post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @place = @post.place
+    @guess = Guess.find(params[:id])
+    @guess.destroy
+    redirect_to place_post_path(@place, @post)
+  end
+
   private
   def guess_params
     params.require(:guess).permit(:guess_text)
