64aa9148c1edfb876d6b3acfaaa9da50e78a4dea
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-05 09:49:45 -0400

    update places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,18 @@
 class PlacesController < ApplicationController
+  #index
   def index
     @places = Place.all
+  end
+  #new
+  def new
+    @place = Place.new
+  end
+  #create
+  #edit
+  #update
+  #destroy
+  private
+  def place_params
+    params.require(:place).permit(:country, :state, :city, :place_photo_url)
   end
 end
