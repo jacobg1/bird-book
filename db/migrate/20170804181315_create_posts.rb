bf238bb22c591b66cb076c19807de62d7c51b08e
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-06 15:39:21 -0400

    add timestamps to post and guesses

--- db/migrate/20170804181315_create_posts.rb
+++ db/migrate/20170804181315_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
     create_table :posts do |t|
       t.string :title
       t.string :photo_url
+      t.timestamps
       t.references :place, index: true, foreign_key: true
     end
   end
--- db/migrate/20170804181324_create_guesses.rb
+++ db/migrate/20170804181324_create_guesses.rb
@@ -2,6 +2,7 @@ class CreateGuesses < ActiveRecord::Migration[5.1]
   def change
     create_table :guesses do |t|
       t.string :guess_text
+      t.timestamps
       t.references :post, index: true, foreign_key: true
     end
   end
