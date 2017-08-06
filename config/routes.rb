eaef5f9ad2e441842a8e80b0873f0ebabcd8657b
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-06 16:16:34 -0400

    add devise files and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'places#index'
 
     resources :places do
@@ -8,7 +9,6 @@ Rails.application.routes.draw do
     resources :posts do
       resources :guesses
     end
-
 
   # resources :places do
   #   resources :posts do
