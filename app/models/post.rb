1b7d3d38c0a422179875658d193e43df1db755cf
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-07 19:35:21 -0400

    add category show

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   has_many :guesses, dependent: :destroy
   belongs_to :place
-  has_many :tags
+  has_many :tags, dependent: :destroy
   has_many :categories, through: :tags
   validates :title, :photo_url, presence: true
 end
