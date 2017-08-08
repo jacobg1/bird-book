c6d07b950555385cf2c89be191497c2cbb30775b
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-08 13:58:46 -0400

    update for tag index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,8 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+  end
+  
   def create
     @post = Post.find(params[:post_id])
     @place = @post.place
@@ -13,4 +17,5 @@ class TagsController < ApplicationController
     end
       redirect_to place_post_path(@place, @post)
     end
+
 end
